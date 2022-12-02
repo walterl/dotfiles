ccfd5a9142189b73236d9f792f5081c386b366d6
Walter Leibbrandt <git@wrl.co.za>
2022-12-02 14:50:38 +0200

    python: Disable ANSI colours in REPL
    It breaks Conjure's ability to parse REPL input/output.

--- startup.py
+++ startup.py
@@ -1,6 +1,7 @@
 import pprint
 import sys
 
-sys.ps1 = "\033[0;34m>>> \033[0m"
-sys.ps2 = "\033[1;34m... \033[0m"
+# The ANSI colours below break Conjure's ability to parse REPL input/output
+# sys.ps1 = "\033[0;34m>>> \033[0m"
+# sys.ps2 = "\033[1;34m... \033[0m"
 sys.displayhook = pprint.pprint
