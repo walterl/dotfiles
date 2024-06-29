e664e910794894ecbd7666f1786595b17076946c
Walter Leibbrandt <git@wrl.co.za>
2024-06-29 22:50:52 +0200

    nvim: Always bind K to LSP hover
    Doesn't always seem to be defined in nvim 0.10.0, as expected.

--- _config/nvim/lua/plugins.lua
+++ _config/nvim/lua/plugins.lua
@@ -697,10 +697,10 @@ require('packer').startup(function(use)
           symbols_opts = { fname_width = 50, symbol_width = 50 }
 
           if is_older_than_nvim_0_10_0 then
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
             vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next({ wrap = false }) end, ext(opts, { desc="LSP Next diagnostic" }))
             vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev({ wrap = false }) end, ext(opts, { desc="LSP Prev diagnostic" }))
           end
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, ext(opts, { desc="LSP hover" }))
           vim.keymap.set('n', 'gd', tel_builtin_fn('lsp_definitions'), ext(opts, { desc="LSP definitions" }))
           vim.keymap.set('n', '<Leader>lD', peek_definition, ext(opts, { desc="Peek definition" }))
           vim.keymap.set('i', '<C-j>', vim.lsp.buf.hover, opts)
