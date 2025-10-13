29cf73837bef8af8d7d2d039148b91b7cc248162
Walter Leibbrandt <git@wrl.co.za>
2025-10-14 01:45:30 +0200

    nvim: Tweak cnofig for vim-dadbod-ui

--- _config/nvim/lua/plugins/base.lua
+++ _config/nvim/lua/plugins/base.lua
@@ -181,7 +181,33 @@ return {
   'stevearc/dressing.nvim',
   'tpope/vim-commentary',
   'tpope/vim-dadbod',
-  'kristijanhusak/vim-dadbod-ui',
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      {
+        'kristijanhusak/vim-dadbod-completion',
+        ft = { 'sql', 'mysql', 'plsql' },
+        lazy = true
+      },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      vim.g.db_ui_execute_on_save = 0
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_hide_schemas = { 'pg_toast.*' }
+      vim.g.db_ui_table_helpers = {
+        postgresql = {
+          Count = 'SELECT COUNT(*) FROM "{table}"'
+        },
+      }
+    end,
+  },
   'tpope/vim-eunuch',
   'tpope/vim-repeat',
   'tpope/vim-sleuth',
