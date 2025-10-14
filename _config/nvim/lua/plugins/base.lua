a0841b25a0d348b480d093e61be580acd612cbca
Walter Leibbrandt <git@wrl.co.za>
2025-10-14 02:03:48 +0200

    nvim: Fix leap.nvim mappings config

--- _config/nvim/lua/plugins/base.lua
+++ _config/nvim/lua/plugins/base.lua
@@ -22,7 +22,7 @@ return {
     'ggandor/leap.nvim',
     config = function()
       require('leap').setup {}
-      require('leap').set_default_keymaps()
+      require('leap').set_default_mappings()
     end,
   },
   {
